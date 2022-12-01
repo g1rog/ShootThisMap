2fdcb180f7d2d23a2de16c3edd2f23b891865624
g1rog <sunnycovex@gmail.com>
2022-12-01 23:30:29 +0700

    Added auto spawn bots

--- Source/ShootThisMap/Private/STMGameModeBase.cpp
+++ Source/ShootThisMap/Private/STMGameModeBase.cpp
@@ -1,5 +1,6 @@
 
 #include "STMGameModeBase.h"
+#include "AIController.h"
 #include "Player/STMBaseCharacter.h"
 #include "Player/STMPlayerController.h"
 
@@ -7,4 +8,28 @@ ASTMGameModeBase::ASTMGameModeBase()
 {
     DefaultPawnClass = ASTMBaseCharacter::StaticClass();
     PlayerControllerClass = ASTMPlayerController::StaticClass();
+}
+
+void ASTMGameModeBase::StartPlay()
+{
+    Super::StartPlay();
+    SpawnBots();
+}
+
+UClass* ASTMGameModeBase::GetDefaultPawnClassForController_Implementation(AController *InController)
+{
+    if (InController && InController->IsA<AAIController>()) return AIPawnClass;
+    return Super::GetDefaultPawnClassForController_Implementation(InController);
+}
+
+void ASTMGameModeBase::SpawnBots()
+{
+    if (!GetWorld()) return;
+    for (int32 i = 0; GameData.PlayersNum - 1; ++i)
+    {
+        FActorSpawnParameters SpawnInfo;
+        SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+        const auto STMAIController = GetWorld()->SpawnActor<AAIController>(AIControllerClass, SpawnInfo);
+        RestartPlayer(STMAIController);
+    }
 }
--- Source/ShootThisMap/Public/STMGameModeBase.h
+++ Source/ShootThisMap/Public/STMGameModeBase.h
@@ -2,14 +2,36 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "STMCoreTypes.h"
 #include "GameFramework/GameModeBase.h"
 #include "STMGameModeBase.generated.h"
+
+class AAIController;
 
 UCLASS()
 class SHOOTTHISMAP_API ASTMGameModeBase : public AGameModeBase
 {
-	GENERATED_BODY()
-
+    GENERATED_BODY()
+    
 public:
     ASTMGameModeBase();
+    virtual void StartPlay() override;
+    virtual UClass* GetDefaultPawnClassForController_Implementation(AController *InController) override;
+    
+protected:
+private:
+    void SpawnBots();
+
+public:
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    TSubclassOf<AAIController> AIControllerClass;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    TSubclassOf<APawn> AIPawnClass;
+    
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    FGameData GameData;
+
+private:
 };
