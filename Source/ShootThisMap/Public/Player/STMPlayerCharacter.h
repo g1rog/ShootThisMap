b734fe51ea8b939f19eb72e69dc821109de912cf
g1rog <106026930+g1rog@users.noreply.github.com>
2022-12-08 20:16:55 +0700

    Update STMPlayerCharacter.h

--- Source/ShootThisMap/Public/Player/STMPlayerCharacter.h
+++ Source/ShootThisMap/Public/Player/STMPlayerCharacter.h
@@ -25,7 +25,7 @@ protected:
 private:
     UFUNCTION()
     void OnCameraCollisionBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) ;
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
     UFUNCTION()
     void OnCameraCollisionEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
